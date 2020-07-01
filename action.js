12a471de3ba56bac010fed0726c8aa59d661c25e
Dor Shahaf <dor@shahaf.com>
2020-07-01 21:35:27 +0300

    And more...

--- action.js
+++ action.js
@@ -23,6 +23,7 @@ module.exports = class {
     const projectId = _.get(origIssue, 'fields.project.id')
     
     const { versions } = await this.Jira.getProjectVersions(projectId)
+    console.log(`All versions:${JSON.stringify(versions, null, 4)}`)
     let versionToApply = _.find(versions, (v) => {
       if (v.name.toLowerCase() === argv.fixVersion.toLowerCase()) return true
     })
