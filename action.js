366d31a04f8236f3230ab2d7a922f249412c3863
yurabrovka <77271927+yurabrovka@users.noreply.github.com>
2021-03-29 15:27:09 +0300

    Update action.js

--- action.js
+++ action.js
@@ -66,7 +66,7 @@ module.exports = class {
         update: {
           fixVersions: [
             {
-              add: {
+              set: {
                 name: argv.fixVersion
               }
             }
